15ab73ac1a57c02d4518065caaa465d38583419b
Satriowish <satriowisnuap@gmail.com>
2024-10-20 08:03:50 +0700

    penambahan style pada halamn index

--- index.php
+++ index.php
@@ -1,77 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
-	<title>Form_kontak</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aplikasi Kontak Modern</title>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/index.css">
 </head>
-<body align="center" style="font-family: arial;">
+<body>
+	<div class="container">
+		<h1>Aplikasi Kontak </h1>
+		
+		<table>
+			<thead>
+				<tr>
+					<th>No.</th>
+					<th>Nama</th>
+					<th>Telpon</th>
+					<th>Email</th>
+					<th>Aksi</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				require_once 'connection.php';
+				$kontak = new kontak();
+				$kontak->connect(); 
+				$kontak->selectAllKontak();
 
-	<h1>APLIKASI FORM KONTAK</h1>
-	<table align="center" border="1">
-		<tr>
-			<th>No.</th>
-			<th>Nama</th>
-			<th>Telpon</th>
-			<th>Email</th>
-			<th>Aksi</th>
-		</tr>
-		<?php 
-			require_once 'connection.php'; // Pastikan connection.php menggunakan sqlsrv
-			$kontak = new kontak();
-			$kontak->connect(); 
-			$kontak->selectAllKontak();
+				$dataKontak = $kontak->get_dataset();
+				$no = 1;
 
-			$dataKontak = $kontak->get_dataset();
-			$no = 1;  // Mulai nomor dari 1
-
-			for ($i = 0; $i < count($dataKontak); $i++) { 
-		 ?>
-		<tr>
-			  <td align="center"><?php echo $no; ?></td>
-		      <td>
-		      	<a href="form_edit_kontak.php?idKontak=<?php echo $dataKontak[$i][0]; ?>"><?php echo $dataKontak[$i][1]; ?></a>
-		      </td>
-		      <td align="center"><?php echo $dataKontak[$i][2]; ?></td>
-		      <td><?php echo $dataKontak[$i][3]; ?></td>
-		      <td align="center">
-		      	<a style="text-decoration:none;color:orange;" href="form_edit_kontak.php?idKontak=<?php echo $dataKontak[$i][0]; ?>"><b>Edit</b></a> | 
-		        <a style="text-decoration:none;color:red;" href="proses/delete_kontak.php?idKontak=<?php echo $dataKontak[$i][0]; ?>"><b>Delete</b></a>
-		      </td>
-		</tr>
-		<?php $no++; } ?>
-	</table>
-	<hr>
-	<form action="proses/insert_kontak.php" method="post">
-		<h2>FORM INPUT KONTAK</h2>
-		<table align="center">
-			<tr>
-				<td>Nama</td>
-				<td><input type="text" name="nama" required=""></td>
-			</tr>
-			<tr>
-				<td>Kontak</td>
-				<td><input type="number" name="kontak" required=""></td>
-			</tr>
-			<tr>
-				<td>Email</td>
-				<td><input type="email" name="email" required=""></td>
-			</tr>
-			<tr>
-				<td colspan="2" align="right">
-					<button type="submit" name="insert" 
-					style="
-					background-color: #123566;
-					color: #fff;
-					padding: 5px 15px;
-					border: none;
-					border-radius: 5px;
-					">Kirim</button>
-				</td>
-			</tr>
+				foreach ($dataKontak as $data) { 
+				?>
+				<tr>
+					<td><?php echo $no++; ?></td>
+					<td><a href="form_edit_kontak.php?idKontak=<?php echo $data[0]; ?>"><?php echo $data[1]; ?></a></td>
+					<td><?php echo $data[2]; ?></td>
+					<td><?php echo $data[3]; ?></td>
+					<td>
+						<a href="form_edit_kontak.php?idKontak=<?php echo $data[0]; ?>" class="btn btn-edit">Edit</a>
+						<a href="proses/delete_kontak.php?idKontak=<?php echo $data[0]; ?>" class="btn btn-delete">Hapus</a>
+					</td>
+				</tr>
+				<?php } ?>
+			</tbody>
 		</table>
-	</form>
-	<nav>
-	<p>&copy;Satrio - 2024</p
-    <p>Created by : Satrio Wisnu Adi Pratama</p>
-	</nav>
+
+		<form action="proses/insert_kontak.php" method="post">
+			<h2>Tambah Kontak Baru</h2>
+			<input type="text" name="nama" placeholder="Nama" required>
+			<input type="number" name="kontak" placeholder="Nomor Telepon" required>
+			<input type="email" name="email" placeholder="Email" required>
+			<button type="submit" name="insert">Tambah Kontak</button>
+		</form>
+	</div>
+
+	<footer>
+		<p>&copy; 2024 Satrio Wisnu Adi Pratama. All rights reserved.</p>
+	</footer>
 </body>
 </html>
