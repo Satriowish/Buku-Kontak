2265375a212a457df0a7fc676ff90656fdee3821
Satriowish <satriowisnuap@gmail.com>
2024-10-20 08:06:20 +0700

    penambahan style pada halaman edit kontak

--- form_edit_kontak.php
+++ form_edit_kontak.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Edit Kontak</title>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/form_edit.css">
 </head>
-<body align="center">
-	<?php 
-		require_once 'connection.php';  // Pastikan connection.php sudah disesuaikan
+<body>
+	<div class="container">
+		<h2>Edit Kontak</h2>
+		<?php 
+			require_once 'connection.php';  // Pastikan connection.php sudah disesuaikan
 
-		// Mengambil ID kontak dari URL
-		$id_kontak = $_GET['idKontak'];
+			// Mengambil ID kontak dari URL
+			$id_kontak = $_GET['idKontak'];
 
-		// Membuat objek kontak dan koneksi ke database
-		$kontak = new kontak();
-		$kontak->connect(); 
-		$kontak->detailDataKontak($id_kontak);  // Mengambil data detail kontak
+			// Membuat objek kontak dan koneksi ke database
+			$kontak = new kontak();
+			$kontak->connect(); 
+			$kontak->detailDataKontak($id_kontak);  // Mengambil data detail kontak
 
-		// Mendapatkan dataset kontak
-		$dataKontak = $kontak->get_dataset();
-	 ?>
-	<form action="proses/edit_kontak.php?idKontak=<?php echo $id_kontak; ?>" method="post">
-		<h2>FORM EDIT KONTAK</h2>
-		<table align="center">
-			<tr>
-				<th>Nama</th>
-				<td><input type="text" name="nama" value="<?php echo $dataKontak[0][1];?>" required=""></td>
-			</tr>
-			<tr>
-				<th>Kontak</th>
-				<td><input type="number" name="kontak" value="<?php echo $dataKontak[0][2];?>" required=""></td>
-			</tr>
-			<tr>
-				<th>Email</th>
-				<td><input type="email" name="email" value="<?php echo $dataKontak[0][3];?>" required=""></td>
-			</tr>
-			<tr>
-				<td colspan="2" align="right">
-					<button type="submit" name="update">Ubah</button>
-				</td>
-			</tr>
-		</table>
-	</form>
+			// Mendapatkan dataset kontak
+			$dataKontak = $kontak->get_dataset();
+		?>
+		<form action="proses/edit_kontak.php?idKontak=<?php echo $id_kontak; ?>" method="post">
+			<div class="form-group">
+				<label for="nama">Nama</label>
+				<input type="text" id="nama" name="nama" value="<?php echo $dataKontak[0][1];?>" required>
+			</div>
+			<div class="form-group">
+				<label for="kontak">Kontak</label>
+				<input type="number" id="kontak" name="kontak" value="<?php echo $dataKontak[0][2];?>" required>
+			</div>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email" value="<?php echo $dataKontak[0][3];?>" required>
+			</div>
+			<button type="submit" name="update">Ubah</button>
+		</form>
+	</div>
 	<nav>
-    <p>&copy;Satrio - 2024</p>
-    <p>Created by : Satrio Wisnu Adi Pratama</p>
+		<p>&copy; 2024 Satrio Wisnu Adi Pratama. All rights reserved.</p>
+	</nav>
 </body>
 </html>
